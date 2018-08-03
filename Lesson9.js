7116dc52003a7420869a6325ff54c3af1c52ade2
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-08-03 20:45:39 +0000

    Lesson9-js

---
+++ Lesson9.js
@@ -0,0 +1,84 @@
+var displayTodosbutton = Document.getElementById('displayTodosbutton');
+
+console.log(displayTodosbutton);
+
+var toggleAllButton = Document.getElementById('toggleAllButton');
+
+displayTodosbutton.addEventListener
+('Click', function() {
+    todosList.displayTodos();
+});
+toggleAllButton.addEventListener
+('Click', function() {
+    togglelist.toggleAll();
+});
+
+// Lesson 8
+
+var handlers = {
+    displayTodos: function() {
+        todosList.displayTodos();
+    },
+     displayTodos: function() {
+        todosList.displayTodos();
+    }
+};
+
+AddTodo: function() {
+    var addTodoTextInput = document.getElementById('addTodoTextInput');
+    todoList.addTodo(addTodoTextInput.value);
+    addTodoTextInput.value = '';
+    view.displayTodo();
+    }
+};
+
+ChangeTodo: function() {
+    var ChangeTodoPositionInput= document.getElementById('ChangeTodoPositionInput');
+    view.displayTodo();
+    var ChangeTodoPositionInput = document.getElementById('ChangeTodoTextInput');
+    todoList.ChangeTodo('ChangeTodoPositionInput.valueAsNumber.');
+    
+    ChangeTodoPositionInput.value = '';
+    ChangeTodoPositionInput.value = '';
+}, 
+
+toggleCompletedPositionInput: function() {
+    var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+    todoList.toggleCompleted('toggleCompletedPositionInput.valueAsNumber');
+    toggleCompletedPositionInput.value = '';
+},
+
+toggleAll: function() {
+    todoList.toggleAll();
+    }
+};
+
+var view = {
+    displayTodos: function() {
+        for (var i = 0; i < todoList.todos.length; i++)
+        todosUL.innerHTML = '';
+        var todosUL = document.querySelector('ul');
+        todoLi.textContent = "todoTextwithcCompletion";
+        todoLi.textContent = "todosList.todos[i].todoText";
+        var todoLi = document.createElement("Li");
+        todoUL.appendChild('todoLi');
+    }
+}
+};,
+
+var todoTextWithCompletion = '';
+ if (todo.Completed === true) {
+     todoTextwithcCompletion = '(x)' + todo.todotext;
+     } else {
+         todoTextwithcCompletion = '()' + todo.todoText;
+     }
+
+
+
+
+
+
+
+
+
+
